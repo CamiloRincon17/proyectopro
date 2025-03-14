8e64ba5f338dd5935311067798846fd0c183c86f
camilo <camilo171512@gmail.com>
2025-03-14 16:23:40 -0500

    .

---
+++ src/HowMany.java
@@ -0,0 +1,10 @@
+public class HowMany {
+    public static void main(String[] args) {
+        int numero=100;
+       for (int i = 1; i <= 100; i++) {
+           System.out.println(i);
+       }
+            
+        }
+    }
+
---
+++ src/IsBigger.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+public class IsBigger{
+public static void main(String[] args) {
+Scanner in = new Scanner(System.in);
+int A=10;
+int B=20;
+int C=5;
+System.out.println("A<B && A<C"+(A<C && A<C)+" ");
+System.out.println("A<B"+(A<B));
+System.out.println("A<C"+(A<C));
+}
+}
